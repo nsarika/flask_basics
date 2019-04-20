627cd0c9ac04e5445e3f96ce05c02716a70c3047
nsarika <n.sarika92@gmail.com>
2019-04-20 22:18:52 +0530

    added some files

---
+++ run.py
@@ -0,0 +1,17 @@
+from app import create_app,db
+from app.auth.models import User
+
+
+if __name__ == '__main__':
+    flask_app = create_app('dev')
+    with flask_app.app_context():
+        db.create_all()
+        if not User.query.filter_by(user_name='sarika').first():
+            User.create_user(user='sarika',
+                             email='sarika@potters.com',
+                             password='secret')
+
+    flask_app.run()
+
+
+
